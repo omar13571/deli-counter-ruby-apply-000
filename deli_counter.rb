47da7240bc6f73bce7f57d2064d5689431a72b9d
peaceful-distribution-8695 <porcaoma.1357@gmail.com>
2019-12-19 18:30:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,14 @@
-katz_deli = []
+katz_deli = [Finn, Rey, Kylo]
 
-take_a_number(katz_deli, "Ada")
-take_a_number(katz_deli, "Grace")
-take_a_number(katz_deli, "Kent")
-
-line(katz_deli)
-
-now_serving(katz_deli)
-
-line(katz_deli)
-
-take_a_number(katz_deli, "Matz")
-
-line(katz_deli)
-
-now_serving(katz_deli)
-
-line(katz_deli)
+def line(arrays)
+  if array.length >= 1
+    nuarray = []
+    counter = 1
+    array.each do |name|
+      nuarray.push("#{{counter}. #{name}")
+    end
+    puts "The line is currently : #{nuarray.join(" ")}
+  else
+    puts "The line is currently empty."
+  end
+end
